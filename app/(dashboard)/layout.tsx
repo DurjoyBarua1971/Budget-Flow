d5c600479a52cee4c5ba24a38e14c0a311ce98a3
Durjoy Barua <durjoy.barua@brainstation-23.com>
2025-05-19 20:20:21 +0600

    Refactor code structure for improved readability and maintainability

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/Navbar";
+import React from "react";
+
+export default function layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex flex-col h-screen w-full relative">
+      <Navbar />
+      <div className="w-full"> {children}</div>
+    </div>
+  );
+}
